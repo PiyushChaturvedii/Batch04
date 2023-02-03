e72e6e01a744c26dd02bd7da485d90980ed08486
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2023-02-03 17:34:32 +0530

    Project4,5

---
+++ Project/JS4/script.js
@@ -0,0 +1,14 @@
+const messageInput = document.getElementById('message-input');
+
+
+messageInput.addEventListener("keydown",function (event) {
+  if (event.key == "Enter")
+    getMessage();
+})
+
+
+function getMessage() {
+  console.log(messageInput.value);
+  document.getElementById("message-output").innerHTML = messageInput.value;
+  messageInput.value = "";
+}
