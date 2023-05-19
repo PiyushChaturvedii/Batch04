f7e8f7000f816c7321b49c180323b1d219f22123
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2023-05-19 18:13:56 +0530

    cart&useradd

---
+++ Nodejs/day28/app.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+const mongoConnect = require('./util/database').mongoConnect;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('6467610eebfe55bdd5f23be6')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(8000);
+})
