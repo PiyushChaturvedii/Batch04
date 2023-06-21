2a9307b66130c61c154c712aa32307b6b314f6bc
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2023-06-21 18:31:18 +0530

    formErrorHandling

---
+++ Nodejs/day40/controllers/auth.js
@@ -0,0 +1,130 @@
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+
+const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+    auth:{
+        api_key: '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'
+    }
+}))
+
+exports.getLogin = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        errorMessage: message
+    });
+}
+
+exports.getSignup = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        errorMessage:message
+    });
+}
+
+exports.postLogin = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                req.flash('error', 'Email not in Database.');
+                return res.redirect('/login');
+            }
+            bcrypt
+                .compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save(err => {
+                            console.log(err);
+                            res.redirect('/');
+                        });
+                    }
+                    req.flash('error', 'Incorrect password.');
+                    return res.redirect('/login');
+                })
+        })
+        .catch(err => console.log(err));
+}
+
+exports.postSignup = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+    User.findOne({ email: email })
+        .then(userDoc => {
+            if (userDoc) {
+            req.flash('error', 'E-Mail already exists, take different one.')
+            return res.redirect('/signup');
+        }
+            return bcrypt
+                .hash(password, 12)
+                .then(hashedPassword => {
+                    const user = new User({
+                    email: email,
+                    password: hashedPassword,
+                    cart: { items: [] }
+                    });
+            return user.save();
+        })
+        }).then(result => {
+            res.redirect('/login')
+            return transporter.sendMail({
+                to: email,
+                from: 'hello@aeeron.in',
+                subject: 'Signup succeeded!',
+                html: '<h1> You successfully signed up!</h1>'
+            })
+    }).catch(err=>console.log(err))
+ };
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    })
+}
+
+exports.getReset = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+        res.render('auth/reset', {
+            path: '/reset',
+            pageTitle: 'Reset Password',
+            errorMessage: message
+        });
+}
+    
+exports.postReset = (req, res, next) => {
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) {
+            console.log(err);
+            return res.redirect('/reset');
+        }
+        const token = buffer.toString('hex');
+        console.log(token)
+    });
+}
