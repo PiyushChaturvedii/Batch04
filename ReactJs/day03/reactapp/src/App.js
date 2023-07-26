0f3328ca5f4b41061b6ffef79037566a182050be
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2023-07-26 18:18:05 +0530

    formsubmit

---
+++ ReactJs/day03/reactapp/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const App= () => {
+ 
+  const expenses = [
+    { id: 'e1', title: 'Milk', amount: 100, date: new Date(2030, 10, 20) },
+    { id: 'e2', title: 'Tea', amount: 200, date: new Date(2033, 11, 20) },
+    { id: 'e3', title: 'Water', amount: 50, date: new Date(2034, 12, 20) },
+    { id: 'e4', title: 'Sugar', amount: 400, date: new Date(2035, 13, 20) }
+  ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started !!!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+
+
+  return (
+    <div>
+      <NewExpense/>
+      <Expenses items={expenses}/>
+    </div>
+  );
+}
+
+export default App;
