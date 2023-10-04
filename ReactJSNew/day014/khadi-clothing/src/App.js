695a5601d9a67d1ff5aa0c63ee7803fa5cc2f244
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2023-10-04 18:54:26 +0530

    Cart

---
+++ ReactJSNew/day014/khadi-clothing/src/App.js
@@ -0,0 +1,27 @@
+import { Routes, Route, Outlet } from 'react-router-dom';
+import "./categories.styles.scss";
+import Home from "./routes/home/home.component";
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
+
+
+// const Shop = () => {
+//   return <h1> I am the shop page </h1>
+// }
+
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path = '/' element = {<Navigation />}>
+        <Route path = 'home' element = {<Home />}/>
+        <Route path = 'shop' element = {<Shop />}/>
+        <Route path='auth' element={<Authentication />}/>
+
+      </Route>
+  </Routes>
+)
+}
+
+export default App;
