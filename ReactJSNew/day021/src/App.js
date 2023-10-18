71c89f7bc6d2edcb8250b91ea88daa831e4fbf9a
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2023-10-18 19:07:54 +0530

    Redux_UserReducer

---
+++ ReactJSNew/day021/src/App.js
@@ -0,0 +1,45 @@
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from './utils/fireebase/firebase.utils';
+
+
+
+import { Routes, Route } from 'react-router-dom';
+import "./categories.styles.scss";
+import Home from "./routes/home/home.component";
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
+import { setCurrentUser } from './store/user/user.action';
+
+
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+    return unsubscribe;
+  }, []);
+
+  return (
+    <Routes>
+      <Route path = '/' element = {<Navigation />}>
+        <Route path = 'home' element = {<Home />}/>
+        <Route path = 'shop/*' element = {<Shop />}/>
+        <Route path='auth' element={<Authentication />}/>
+        <Route path='checkout' element={<Checkout />}/>
+
+      </Route>
+  </Routes>
+)
+}
+
+export default App;
